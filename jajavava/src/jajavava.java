f80fc5edaab4cdabca6940717386405a99eaf285
Pzhoon <pjhckj@naver.com>
2023-02-16 17:46:44 +0900

    11

---
+++ jajavava/src/jajavava.java
@@ -0,0 +1,6 @@
+public class jajavava {
+    public static void main(String[] args) throws Exception {
+        System.out.println("Hello, jajavava");
+        System.out.println("jajavava");
+    }
+}
