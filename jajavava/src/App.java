ed91ba56e36c4bc83e22fbd51241ae3bf56b4cc5
Pzhoon <pjhckj@naver.com>
2023-02-16 17:41:45 +0900

    1

--- jajavava/src/App.java
+++ jajavava/src/App.java
@@ -1,6 +1,6 @@
 public class App {
     public static void main(String[] args) throws Exception {
         System.out.println("Hello, World!");
-        System.out.println("Rearlly Hate Java");
+        System.out.println("Shit");
     }
 }
